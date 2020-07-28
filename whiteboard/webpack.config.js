75cf95ed34ce46d7ff067bff228ebbea05d3d057
rick <rick@netless.link>
2020-07-28 15:01:10 +0800

    antd theme ready

--- whiteboard/webpack.config.js
+++ whiteboard/webpack.config.js
@@ -68,7 +68,17 @@ config = {
         }, {
           loader: 'css-loader' // translates CSS into CommonJS
         }, {
-          loader: 'less-loader' // compiles Less to CSS
+          loader: 'less-loader',
+          options: {
+            lessOptions: {
+              modifyVars: {
+                'primary-color': '#1DA57A',
+                'link-color': '#1DA57A',
+                'border-radius-base': '2px',
+              },
+              javascriptEnabled: true
+            }
+          }
         }]
       },
       {
