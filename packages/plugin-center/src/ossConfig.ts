95db14b609468b3e43b54f984ae37f2dcf99676b
rick <rick@netless.link>
2020-08-11 17:39:57 +0800

    fix oss bug

--- packages/plugin-center/src/ossConfig.ts
+++ packages/plugin-center/src/ossConfig.ts
@@ -10,8 +10,8 @@ export type OSSConfigObjType = {
 export const ossConfigObj: OSSConfigObjType = {
     accessKeyId: "LTAI4Fv7DfKEtR347iL3uDmu",
     accessKeySecret: "ycdfrOnz0a3MbDzi9Px4fqhXfKWqsy",
-    region: "beings",
-    bucket: "oss-cn-hangzhou",
+    region: "oss-cn-hangzhou",
+    bucket: "beings",
     folder: "test",
     prefix: "https://beings.oss-cn-hangzhou.aliyuncs.com/",
 };
