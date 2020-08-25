f076251dbfc4a0004797665823d3e3c93677d062
rick <rick@netless.link>
2020-08-25 13:47:38 +0800

    add new state

--- packages/page-controller/src/index.tsx
+++ packages/page-controller/src/index.tsx
@@ -2,8 +2,12 @@ import * as React from "react";
 import {Room, RoomState} from "white-web-sdk";
 import * as next from "./image/next.svg";
 import * as nextDisabled from "./image/next-disabled.svg";
-import * as last from "./image/last.svg";
-import * as lastDisabled from "./image/last-disabled.svg";
+import * as back from "./image/back.svg";
+import * as backDisabled from "./image/back-disable.svg";
+import * as first from "./image/first-active.svg";
+import * as firstDisabled from "./image/first-disable.svg";
+import * as last from "./image/last-active.svg";
+import * as lastDisabled from "./image/last-disable.svg";
 import "./index.less";
 
 export type PageControllerProps = {
@@ -50,17 +54,50 @@ export default class PageController extends React.Component<PageControllerProps,
         );
     }
 
+    private isFirst = (): boolean => {
+        const {roomState} = this.state;
+        const activeIndex = roomState.sceneState.index;
+        return activeIndex === 0;
+    }
+
+    private isLast = (): boolean => {
+        const {roomState} = this.state;
+        const activeIndex = roomState.sceneState.index;
+        const lastIndex = roomState.sceneState.scenes.length - 1;
+        return activeIndex === lastIndex;
+    }
+
+    private setLastStep = (): void => {
+        const {room} = this.props;
+        const {roomState} = this.state;
+        const lastIndex = roomState.sceneState.scenes.length - 1;
+        room.setSceneIndex(lastIndex);
+    }
+
+    private setFirstStep = (): void => {
+        const {room} = this.props;
+        room.setSceneIndex(0);
+    }
+
     public render(): React.ReactNode {
         return (
             <div className="whiteboard-annex-box">
+                <div onClick={() => this.setFirstStep()}
+                     className="whiteboard-annex-arrow-left">
+                    <img src={this.isFirst() ? firstDisabled : first}/>
+                </div>
                 <div onClick={() => this.handlePptPreviousStep()}
                      className="whiteboard-annex-arrow-left">
-                    <img src={last}/>
+                    <img src={this.isFirst() ? backDisabled : back}/>
                 </div>
                 {this.pageNumber()}
                 <div onClick={() => this.handlePptNextStep()}
                      className="whiteboard-annex-arrow-right">
-                    <img src={next}/>
+                    <img src={this.isLast() ? nextDisabled : next}/>
+                </div>
+                <div onClick={() => this.setLastStep()}
+                     className="whiteboard-annex-arrow-left">
+                    <img src={this.isLast() ? lastDisabled : last}/>
                 </div>
             </div>
         );
