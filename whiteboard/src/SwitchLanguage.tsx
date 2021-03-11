481f66922c10364c0d2fa8c85677c3a9269d3ca2
cheerchen <meetcheerego@gmail.com>
2021-03-11 18:32:14 +0800

    chore: upgrade antd & fix swicth language

--- whiteboard/src/SwitchLanguage.tsx
+++ whiteboard/src/SwitchLanguage.tsx
@@ -3,27 +3,26 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { languagesWithName } from "./i18n";
 
-export interface SwitchLanguageProps {
-  id?: string;
-}
-
-export const SwitchLanguage: React.FC<SwitchLanguageProps> = ({ id }) => {
-  const { i18n } = useTranslation();
-  const onChange = (lang: string) => i18n.changeLanguage(lang);
-
-  return (
-    <Select
-      id={id}
-      defaultValue={i18n.language}
-      size="small"
-      style={{ width: 100 }}
-      onChange={onChange}
-    >
-      {languagesWithName.map(({ lang, name }) => (
-        <Select.Option key={lang} value={lang}>
-          {name}
-        </Select.Option>
-      ))}
-    </Select>
-  );
-};
+export const SwitchLanguage: React.FC = React.memo(() => {
+    const { i18n } = useTranslation();
+    console.log("i18n", i18n, languagesWithName);
+    (window as any).i18n = i18n;
+    const onChange = (lang: string) => {
+        if (!lang) return;
+        i18n.changeLanguage(lang);
+    };
+    return (
+        <Select
+            value={i18n.language}
+            size="small"
+            style={{ width: 100 }}
+            onSelect={onChange}
+        >
+            {languagesWithName.map(({ lang, name }) => (
+                <Select.Option key={lang} value={lang}>
+                    {name}
+                </Select.Option>
+            ))}
+        </Select>
+    );
+});
