3953314e69fc770112740aa9f7d5ae91db96cf31
hyrious <hyrious@outlook.com>
2021-06-01 15:43:00 +0800

    chore(video-js-plugin): targeting ES5

--- packages/video-js-plugin/scripts/build.js
+++ packages/video-js-plugin/scripts/build.js
@@ -1,17 +1,17 @@
 const { build } = require("esbuild");
 const pkg = require("../package.json");
 
-build({
-    entryPoints: ["src/index.ts"],
-    platform: "browser",
-    bundle: true,
-    target: "es2018",
-    external: Object.keys(pkg.peerDependencies),
-    format: "cjs",
-    outfile: pkg.main,
-    minify: true,
-    sourcemap: true,
-});
+// build({
+//     entryPoints: ["src/index.ts"],
+//     platform: "browser",
+//     bundle: true,
+//     target: "es2018",
+//     external: Object.keys(pkg.peerDependencies),
+//     format: "cjs",
+//     outfile: pkg.main,
+//     minify: true,
+//     sourcemap: true,
+// });
 
 build({
     entryPoints: ["src/index.ts"],
