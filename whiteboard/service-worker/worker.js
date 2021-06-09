58899436ee3e3df868f746d160bae27b566c5a23
leavesster <11785335+leavesster@users.noreply.github.com>
2021-06-10 02:14:33 +0800

    add multiple fetch

--- whiteboard/service-worker/worker.js
+++ whiteboard/service-worker/worker.js
@@ -22,8 +22,10 @@ self.onfetch = function(event) {
       return cache.match(event.request).then(function(response) {
         if (response) {
           console.log("catch success: ", event.request);
+          return response
+        } else {
+          return fetchMultiple([request.url, request.url.replace("https://convertcdn.netless.link", "https://ap-convertcdn.netless.link")])
         }
-        return response || fetch(event.request);
       });
     }));
   }
@@ -33,4 +35,43 @@ var cachePromise;
 function openCache() {
   if (!cachePromise) { cachePromise = caches.open('netless'); }
   return cachePromise;
+}
+
+function fetchMultiple(urls, init) {
+  return new Promise((resolve, reject) => {
+      let list = [];
+      const fetchResolve = (response) => {
+          console.log("success: ", response.url);
+          resolve(response);
+          list.forEach((v) => {
+              v.abort();
+          });
+      };
+      const rejectList = [];
+      list = urls.map(v => fetchWithAbort(v, init, fetchResolve, e => {
+          rejectList.push(e);
+          if (rejectList.length === list.length) {
+              console.log("fetch all fail");``
+              reject();
+          }
+      }));
+  });
+}
+
+function fetchWithAbort(url, init, resolve, reject) {
+  var controller = new AbortController();
+  var signal = controller.signal;
+  fetch(url, {signal, ...init}).then(function(response) {
+      if (response.status >= 200 && response.status < 400) {
+          return response;
+      } else {
+          reject(new Error("not correct code"));
+      }
+  }).then(res => {
+      resolve(res);
+  }).catch(function(e) {
+      console.log("abort: ", e.name, url);
+      reject(e);
+  });
+  return controller;
 }
