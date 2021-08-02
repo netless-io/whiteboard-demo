8ace6ed69e1f6a777a7f10ea4ff97ab73f28b897
hyrious <hyrious@outlook.com>
2021-08-02 17:02:43 +0800

    refactor float link

--- whiteboard/src/FloatLink.tsx
+++ whiteboard/src/FloatLink.tsx
@@ -1,23 +1,41 @@
 import * as React from "react";
 import "./FloatLink.less";
-import gift from "./assets/image/gift.svg";
+import logo from "./assets/image/logo.png";
+import clock from "./assets/image/clock.svg";
+import close from "./assets/image/close.svg";
+// import gift from "./assets/image/gift.svg";
 import { withTranslation, WithTranslation } from "react-i18next";
 
-class FloatLink extends React.Component<WithTranslation, {}> {
+class FloatLink extends React.Component<WithTranslation, { visible: boolean }> {
     public constructor(props: WithTranslation) {
         super(props);
+        this.state = { visible: true };
     }
+
     public render(): React.ReactNode {
         const { t, i18n } = this.props;
+        const href = `https://sso.agora.io/${i18n.language === "zh-CN" ? "cn" : "en"}/v3/signup`;
+        if (!this.state.visible) return null;
         return (
-            <a href={`https://sso.agora.io/${i18n.language === "zh-CN" ? "cn" : "en"}/v3/signup`} target={"_blank"}>
-                <div className="float-link">
-                    <img src={gift} alt="gift" />
-                    <div className="float-link-text">{t('free-10000')}</div>
-                </div>
-            </a>
+            <div className="float-link">
+                <img src={logo} alt="agora" />
+                <span className="float-link-content">
+                    <img src={clock} alt="" />
+                    <div className="float-link-text">{t("free-10000")}</div>
+                    <a href={href} target="_blank">
+                        {t("get-it")}
+                    </a>
+                </span>
+                <span className="float-link-close" onClick={this.hide}>
+                    <img src={close} alt="close" />
+                </span>
+            </div>
         );
     }
+
+    private hide = () => {
+        this.setState({ visible: false });
+    };
 }
 
 export default withTranslation()(FloatLink);
