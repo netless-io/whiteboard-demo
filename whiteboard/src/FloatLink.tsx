b59232042c58a6b294ab9f3fcf8a3ac09d24f940
cheerchen <meetcheerego@gmail.com>
2021-08-10 15:42:09 +0800

    refactor(whiteboard): adjust float link style

--- whiteboard/src/FloatLink.tsx
+++ whiteboard/src/FloatLink.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import "./FloatLink.less";
-import logo from "./assets/image/logo.png";
 import clock from "./assets/image/clock.svg";
 import close from "./assets/image/close.svg";
+// import logo from "./assets/image/logo.png";
 // import gift from "./assets/image/gift.svg";
 import { withTranslation, WithTranslation } from "react-i18next";
 
@@ -14,11 +14,12 @@ class FloatLink extends React.Component<WithTranslation, { visible: boolean }> {
 
     public render(): React.ReactNode {
         const { t, i18n } = this.props;
-        const href = `https://sso.agora.io/${i18n.language === "zh-CN" ? "cn" : "en"}/v3/signup`;
+        const href = `https://sso.agora.io/${
+            i18n.language === "zh-CN" ? "cn" : "en"
+        }/v3/signup`;
         if (!this.state.visible) return null;
         return (
             <div className="float-link">
-                <img src={logo} alt="agora" />
                 <span className="float-link-content">
                     <img src={clock} alt="" />
                     <div className="float-link-text">{t("free-10000")}</div>
