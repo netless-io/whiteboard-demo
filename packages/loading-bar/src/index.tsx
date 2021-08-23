5c20ffdd668003a38954374193b40039c784fd19
Tao Zeyu <i@taozeyu.com>
2021-08-23 16:32:32 +0800

    fix: shouldn't call setInterval

--- packages/loading-bar/src/index.tsx
+++ packages/loading-bar/src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import TweenOne from "rc-tween-one";
 import "./index.less";
-const timeout = (ms: any) => new Promise(res => setTimeout(res, ms));
 
 export type TopLoadingBarProps = {
     loadingPercent: number;
@@ -9,44 +8,27 @@ export type TopLoadingBarProps = {
 };
 
 export type TopLoadingBarStates = {
-    displayState: string;
+    lastPercent: number;
+    continuousTimer: any;
 };
+
 export default class TopLoadingBar extends React.Component<TopLoadingBarProps, TopLoadingBarStates> {
 
-    private setInte: any;
     public constructor(props: TopLoadingBarProps) {
         super(props);
         this.state = {
-            displayState: "none",
+            lastPercent: 100,
+            continuousTimer: null,
         };
     }
 
-    public componentDidMount(): void {
-        this.setInte = setInterval(async () => {
-            await this.loadingDisplay();
-        }, 100);
-    }
-
-    public componentWillUnmount(): void {
-        clearInterval(this.setInte);
-    }
-
-    private loadingDisplay = async (): Promise<void> => {
-        const isLoadingDone = this.props.loadingPercent === 100;
-        if (isLoadingDone) {
-            await timeout(500);
-            this.setState({displayState: "none"});
-        } else {
-            this.setState({displayState: "flex"});
-        }
-    }
     public render(): React.ReactNode {
         return (
             <TweenOne
                 animation={{
                     duration: 150,
                     width: `${this.props.loadingPercent}%`,
-                    display: this.state.displayState,
+                    display: (this.props.loadingPercent === 100) ? "none" : "flex",
                 }}
                 style={{...this.props.style}}
                 className={"process-top-bar"}
